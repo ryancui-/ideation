89d3297084489e5e36297be2bd50dae263668d0c
ryancui- <cuizhaowei92@gmail.com>
2019-03-01 12:01:33 +0800

    feat: 加上 edit 的菜单选项

--- src/main/index.js
+++ src/main/index.js
@@ -38,6 +38,19 @@ function createWindow() {
 
 function createMenu() {
   const template = [{
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { role: 'pasteandmatchstyle' },
+      { role: 'delete' },
+      { role: 'selectall' }
+    ]
+  }, {
     role: 'window',
     submenu: [{
       role: 'minimize'
